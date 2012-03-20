b6479e5f112dff333bd4ba406ded5b3dde7838db
Mads Nielsen <inkspot84@gmail.com>
2012-03-20 14:19:43 +0100

    Modified pom for test

--- src/main/java/net/praqma/prqa/reports/PRQAQualityReport.java
+++ src/main/java/net/praqma/prqa/reports/PRQAQualityReport.java
@@ -7,28 +7,37 @@ package net.praqma.prqa.reports;
 import net.praqma.jenkins.plugin.prqa.PrqaException;
 import net.praqma.prqa.PRQAContext;
 import net.praqma.prqa.PRQAStatus;
+import net.praqma.prqa.parsers.QualityReportParser;
+import net.praqma.prqa.products.QAR;
+import net.praqma.prqa.status.PRQAQualityStatus;
 
 /**
  *
  * @author Praqma
  */
-public class PRQAQualityReport extends PRQAReport {
+public class PRQAQualityReport extends PRQAReport<PRQAQualityStatus,String> {
+    
+    public PRQAQualityReport(QAR qar) {
+        this.qar = qar;
+        this.parser = new QualityReportParser();
+    }
+    
     public static String getNamingTemplate() {
         return PRQAContext.QARReportType.Quality.toString() + " "+ PRQAReport.XHTML_REPORT_EXTENSION;
     }
-
+    
     @Override
     public String getFullReportPath() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public PRQAStatus completeTask(Object parameter) throws PrqaException {
+    public PRQAQualityStatus completeTask(String parameter) throws PrqaException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public PRQAStatus completeTask() throws PrqaException {
+    public PRQAQualityStatus completeTask() throws PrqaException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
