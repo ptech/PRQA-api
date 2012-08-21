f5b66742afb7a8c0ec7dc10aa64145ffecd8f382
Mads Nielsen <inkspot84@gmail.com>
2012-08-21 15:32:29 +0200

    Added code upload settings

---
+++ src/main/java/net/praqma/prqa/CodeUploadSetting.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.praqma.prqa;
+
+/**
+ *
+ * @author Praqma
+ */
+public enum CodeUploadSetting {
+    AllCode,
+    None,
+    OnlyNew
+}
